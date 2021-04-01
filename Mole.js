908dc428a82518d733abb1fe3930cdef98ce1063
angadsinghkuwar <angadsinghkuwar@gmail.com>
2021-04-01 17:59:02 +0530

    CSS & JS update

---
+++ Mole.js
@@ -0,0 +1,78 @@
+function gerSadInterval() {
+    return Date.now() + 1000;
+}
+
+const moles =[
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-0') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-0') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-0') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-1') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-2') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-3') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-4') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-5') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-6') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-7') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-8') 
+    },
+    { 
+        status : "sad",
+        next : getSadInterval(),
+        king:false,
+        Node:document.getElementById('hole-9') 
+    },
+];
