9d5612c421568770b71b823717d21118d0936946
angadsinghkuwar <angadsinghkuwar@gmail.com>
2021-04-02 21:16:03 +0530

    js update (2)

--- Mole.js
+++ Mole.js
@@ -1,78 +1,119 @@
-function gerSadInterval() {
+function getSadInterval() {
     return Date.now() + 1000;
 }
 
-const moles =[
+function getGoneInterval(){
+    return Date.now() + Math.floor(Math.random() * 18000) +2000;
+}
+function getHungryInterval () {
+    return Date.now() + Math.floor(Math.random()* 3000) +2000;
+}
+const moles =  [
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-0') 
+        node:document.getElementById('hole-0') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-0') 
+        node:document.getElementById('hole-1') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-0') 
+        node:document.getElementById('hole-2') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-1') 
+        node:document.getElementById('hole-3') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-2') 
+        node:document.getElementById('hole-4') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-3') 
+        node:document.getElementById('hole-5') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-4') 
+        node:document.getElementById('hole-6') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-5') 
+        node:document.getElementById('hole-7') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-6') 
+        node:document.getElementById('hole-8') 
     },
     { 
         status : "sad",
         next : getSadInterval(),
         king:false,
-        Node:document.getElementById('hole-7') 
-    },
-    { 
-        status : "sad",
-        next : getSadInterval(),
-        king:false,
-        Node:document.getElementById('hole-8') 
-    },
-    { 
-        status : "sad",
-        next : getSadInterval(),
-        king:false,
-        Node:document.getElementById('hole-9') 
+        node:document.getElementById('hole-9') 
     },
 ];
+
+function getNextStatus (mole){
+    //console.log(mole.node.children[0].classList);
+    switch(mole.status){
+        case "sad":
+            mole.next = getSadInterval();
+            mole.status = "leaving";
+            mole.node.children[0].src ='./mole-leaving.png';
+            break;
+        case "leaving":
+            mole.next = getGoneInterval();
+            mole.status = 'gone';
+            mole.node.children[0].classList.add("gone");
+            break;
+        case "gone":
+            mole.status = 'hungry';
+            mole.next = getHungryInterval();
+            mole.node.children[0].classList.add("hungry");
+            mole.node.children[0].classList.add("gone");
+            mole.node.children[0].src = './mole-hungry.png';
+            break;
+        case "hungry":
+            mole.status = "sad";
+            mole.next = getSadInterval();
+            mole.node.children[0].classList.remove("hungry");
+            mole.node.children[0].src  = './mole-sad.png';
+            break;
+    }
+}
+
+let runAgainAt = Date.now() +100;
+function nextFrame () {
+    const now = Date.now();
+
+    
+    if (runAgainAt <= now){
+        for(let i = 0 ; i < moles.length;i++) {
+            if(moles[i].next <= now) {
+             getNextStatus(moles[i]);
+            }
+        }
+        runAgainAt = now + 100;
+    }
+    requestAnimationFrame(nextFrame);
+}
+
+nextFrame();    
